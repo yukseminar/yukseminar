6b2772eb01d072876da76ead86a6bb34e16a0725
GoldyWy <goldywidiyanto@gmail.com>
2020-05-20 16:50:18 +0700

    pagnation more halaman index

--- application/views/templates/header_user.php
+++ application/views/templates/header_user.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="url" content="<?= base_url() ?>">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title><?= $title; ?></title>
   <!-- SOURCE CSS -->
